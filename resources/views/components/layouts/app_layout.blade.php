82d80fcaa1aeb0c66f4b1a0c787dbb17b577055a
vagrant <vagrant@homestead>
2023-03-16 07:28:36 +0000

    added footer

--- resources/views/components/layouts/app_layout.blade.php
+++ resources/views/components/layouts/app_layout.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title> {{$title}} </title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="{{ secure_asset('/css/app_layout.css') }}" />
-    </head>
-    <body>
+
+<head>
+    <title> {{ $title }} </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="{{ secure_asset('/css/app_layout.css') }}" />
+</head>
+
+<body>
+    <div class="app-container">
         <x-layouts.nav.navbar>
 
         </x-layouts.nav.navbar>
-        <main>{{$slot}}</main>
-    </body>
+        <main>{{ $slot }}</main>
+        <x-layouts.nav.footer></x-layouts.nav.footer>
+    </div>
+</body>
+
 </html>
