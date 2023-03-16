8166ed3cd66ebc6bedbf8f9fb0e6ab1af0869289
vagrant <vagrant@homestead>
2023-03-16 08:09:50 +0000

    added best sellers content section

---
+++ resources/views/components/layouts/content/best-seller.blade.php
@@ -0,0 +1,35 @@
+<h3>Don't let our best deals <span>MELT AWAY!</span></h3>
+<div class="best-seller-container">
+    <div class="product-container">
+        <picture>
+            <img />
+        </picture>
+        <a>
+            <p>Bogoya Splash Special</p>
+        </a>
+    </div>
+    <div class="product-container">
+        <picture>
+            <img />
+        </picture>
+        <a>
+            <p>Cream Liqueur Special</p>
+        </a>
+    </div>
+    <div class="product-container">
+        <picture>
+            <img />
+        </picture>
+        <a>
+            <p>Weed Cookie Special</p>
+        </a>
+    </div>
+    <div class="product-container">
+        <picture>
+            <img />
+        </picture>
+        <a>
+            <p>BYOT Special</p>
+        </a>
+    </div>
+</div>
