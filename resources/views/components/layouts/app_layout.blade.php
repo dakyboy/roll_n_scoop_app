3a8245c20f6e601274ff03864ccd97384f2f6034
vagrant <vagrant@homestead>
2023-03-16 06:51:19 +0000

    added navbar

--- resources/views/components/layouts/app_layout.blade.php
+++ resources/views/components/layouts/app_layout.blade.php
@@ -3,10 +3,13 @@
     <head>
         <title> {{$title}} </title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-sclae=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="{{ secure_asset('/css/app_layout.css') }}" />
     </head>
     <body>
+        <x-layouts.nav.navbar>
+
+        </x-layouts.nav.navbar>
         <main>{{$slot}}</main>
     </body>
 </html>
